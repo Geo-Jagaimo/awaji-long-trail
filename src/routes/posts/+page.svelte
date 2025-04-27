cf3c74f5f839bdd9827de3c8a0723ce2c2522113
Geo-Jagaimo <geo.jagaimo@gmail.com>
2025-04-28 03:33:00 +0900

    posts card style

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+
 	let { data } = $props<{ data: PageData }>();
 </script>
 
@@ -7,19 +8,32 @@
 	<title>Awaji Long Trail</title>
 	<meta
 		name="description"
-		content="Awaji Long Trail is a scenic hiking route that showcases the natural beauty of Awaji Island."
+		content="ALTの活動記録です。"
 	/>
 </svelte:head>
 
-<div class="justify-centerp-6 flex flex-col items-center">
-	<div class="mx-auto mt-32 mb-6 text-3xl font-bold lg:max-w-[1080px]">活動記録</div>
-	<ul class="space-y-2">
-		{#each data.contents as content (content.id)}
-			<li>
-				<a href={`/posts/${content.id}`} class="text-blue-600 hover:underline">
-					{content.title}
-				</a>
-			</li>
-		{/each}
-	</ul>
+<div class="flex flex-col items-center p-6">
+    <div class="mx-auto w-full max-w-[1080px] px-6 py-6">
+        <div
+            class="text-2xl font-bold leading-tight text-amber-800 md:text-4xl lg:text-5xl"
+            style="text-spacing-trim: trim-start;"
+        >
+            活動記録
+        </div>
+    </div>
+	<div class="grid w-full max-w-[1080px] grid-cols-1 gap-6 px-6 lg:grid-cols-3">
+        {#each data.contents as content (content.id)}
+            <a 
+                href={`/posts/${content.id}`} 
+                class="block rounded-lg border border-gray-200 bg-white p-12 shadow transition hover:shadow-lg"
+            >
+                <h2 
+                    class="text-md font-bold text-amber-800 md:text-lg"
+                    style="text-spacing-trim: trim-start;"
+                >
+                    {content.title}
+                </h2>
+            </a>
+        {/each}
+    </div>
 </div>
