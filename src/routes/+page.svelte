a0c6d203ccdc9b17200bf6e0076ba66eaa8b4d59
Geo-Jagaimo <geo.jagaimo@gmail.com>
2025-04-28 02:50:59 +0900

    text color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,20 +14,22 @@
 
 	<!-- テキストブロック -->
 	<div class="absolute top-0 left-0 flex h-[600px] w-full items-center justify-center">
-		<div class="flex w-full max-w-[1080px] flex-col px-6 text-slate-800">
+		<div class="flex w-full max-w-[1080px] flex-col px-6">
 			<div
-				class="text-2xl leading-tight font-bold md:text-4xl lg:text-5xl"
+				class="text-2xl leading-tight font-bold text-amber-800 md:text-4xl lg:text-5xl"
 				style="text-spacing-trim: trim-start;"
 			>
 				「歩くアワイチ」を目指して
 			</div>
-			<div class="text-md mt-8 font-bold md:text-lg" style="text-spacing-trim: trim-start;">
+			<div
+				class="text-md mt-8 font-bold text-amber-800 md:text-lg"
+				style="text-spacing-trim: trim-start;"
+			>
 				かつて、木炭の産地だった竹原。<br />
 				集落の四方を囲む山林には、立派な炭窯跡と、それらを繋ぐ山林道のネットワークが残っています。<br
 				/>
-				「淡路島ロングトレイル構想」は、そんな山林に少しだけ手を加えてトレイルコースとして整備し、<br
+				「淡路島ロングトレイル構想」は、そんな山林に少しだけ手を加えてトレイルコースとして整備し、竹原の魅力を味わってもらう取り組みです。<br
 				/>
-				竹原の魅力を味わってもらう取り組みです。<br />
 				淡路島全土を歩いて繋ぐ「歩くアワイチ」を目指して、ゆっくりと活動を進めています。
 			</div>
 		</div>
